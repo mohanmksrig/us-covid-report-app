01c2e3a0d2c1ffb082ea564ec7037f936df30882
Mohankumar Selvaraj <mohanmksri@gmail.com>
2024-10-28 15:50:29 +0100

    Final Commit for this Application

--- app/api/covid/route.js
+++ app/api/covid/route.js
@@ -10,9 +10,8 @@ export async function GET() {
     const covidData = await sql`SELECT * FROM CovidData`;
     return NextResponse.json({ covidData: covidData.rows }, { status: 200 });
   } 
-  // eslint-disable-next-line no-unused-vars
-  catch (error) {
-    // eslint-disable-next-line no-unused-vars
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
+  catch (_) {
     return NextResponse.json({ error: 'Failed to fetch data' }, { status: 500 });
   }
 }
@@ -30,9 +29,8 @@ export async function POST(request) {
     `;
     return NextResponse.json({ message: 'Data inserted successfully' }, { status: 201 });
   } 
-  // eslint-disable-next-line no-unused-vars
-  catch (error) {
-    // eslint-disable-next-line no-unused-vars
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
+  catch (_) {
     return NextResponse.json({ error: 'Failed to insert data' }, { status: 500 });
   }
 }
